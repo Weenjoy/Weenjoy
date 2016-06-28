473a0b8714a5363b3569147d55535e1a6cb1dd21
HellonJoven <840962684@qq.com>
2016-06-28 18:58:26 +0800

    加入了跳过开始动画并修改了登录注册界面

--- app/src/main/java/com/df/dianping/SplashActivity.java
+++ app/src/main/java/com/df/dianping/SplashActivity.java
@@ -6,21 +6,62 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 import com.eftimoff.androipathview.PathView;
 
 public class SplashActivity extends Activity {
     private MyVideoView vvStart;
-
-    public void onCreate(Bundle savedInstanceState)
+    private Button btJump;
+    private int time=5;
+    //主线程
+    private Handler mainHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg){
+            if(msg.what==0){
+                btJump.setText("跳过（"+time+"s)");
+                time--;
+                if(time==0){
+                    btJump.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setClass(SplashActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            SplashActivity.this.finish();
+                        }
+                    });
+                }
+            }
+        }
+    };
+    //倒数跳过
+    private Thread thJump=new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()&&time>=0) {
+                mainHandler.obtainMessage(0).sendToTarget();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    });
+        public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
     /* 加载start.xml Layout */
         setContentView(R.layout.activity_splash);
     /* 设定VideoView */
         vvStart = (MyVideoView) findViewById(R.id.vv_start);
+        btJump=(Button)findViewById(R.id.bt_jump);
         Uri uri = Uri.parse
                 (
                         "android.resource://"+getPackageName()+"/"+ R.raw.startvideo
@@ -42,6 +83,8 @@ public class SplashActivity extends Activity {
                 SplashActivity.this.finish();
             }
         });
+
+        thJump.start();
     }
 
     /*private PathView pathView;
